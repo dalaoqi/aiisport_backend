4f24eeceb50e58dc23a452293d0123b061acca98
dalaoqi <mystes3016@gmail.com>
2025-02-26 23:46:30 +0800

    fix: add logging for token exchange and user info retrieval errors

--- main.go
+++ main.go
@@ -389,6 +389,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	code := r.URL.Query().Get("code")
 	token, err := oauthConfig.Exchange(context.Background(), code)
 	if err != nil {
+		log.Fatalf("Failed to exchange token: %v", err)
 		http.Error(w, "Failed to get access token", http.StatusInternalServerError)
 		return
 	}
@@ -397,6 +398,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	client := oauthConfig.Client(context.Background(), token)
 	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
 	if err != nil {
+		log.Fatalf("Failed to get user info: %v", err)
 		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
 		return
 	}
